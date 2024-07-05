f0daa7d2fc95972a2d7e977c4ca8521e7c5632a4
christofilth <171975741+christofilth@users.noreply.github.com>
2024-07-05 20:14:26 +0100

    Make function for divide
    Created divide function. Added if statement so that if the user tries
    to divide by zero, it returns an error.
    
    Removed console logs after confirming each function works.

--- script.js
+++ script.js
@@ -2,16 +2,17 @@ function add(a, b){
     return a+b;
 }
 
-console.log(add(5, 3));
-
 function subtract(a, b){
     return a-b;
 }
-
-console.log(subtract(5, 3));
 
 function multiply(a, b){
     return a*b;
 }
 
-console.log(multiply(5, 3));
+function divide(a, b){
+    if (b === 0){
+        return "ERROR: Can't divide by zero."
+    }
+    return a/b;
+}
